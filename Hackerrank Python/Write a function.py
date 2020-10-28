29208dae73de49323beab8455657972f67baddb2
Md. Imrul Kayes <md.tushar1804@gmail.com>
2020-10-28 23:36:48 +0600

    commit-1 25/10/20

---
+++ Hackerrank Python/Write a function.py
@@ -0,0 +1,11 @@
+
+
+def leap_year(year):
+    if ((year % 4 == 0 ) and (year % 100 != 0) ) or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+y = int(input())
+print(leap_year(y))
